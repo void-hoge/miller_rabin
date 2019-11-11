1f99aeb485fd03757c1bbb6f52baaf6923d1afa9
voidhoge <coilgunrailgun@gmail.com>
2019-11-12 02:12:14 +0900

    added enumerate mode
    changed display format, added enum mode, updated readme

--- README.md
+++ README.md
@@ -1,8 +1,88 @@
-## Miller-Rabin Primality Test
-実装しただけ  
-https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test  
-　以下のようにコマンドラインから入力する。  
-` $ ./isprime.out 89`  
-　素数なら`prime`, 合成数もしくは1以下の整数ならば`composite`と表示される。  
-数字でないものが含まれている場合、内部的には0になるので`composite`になる。  (mpz_get_strを使っている関係)  
-　多倍長整数の演算にはGNU Multi Precision Arithmetic Library(https://gmplib.org/ )を使っている。コンパイルにはGMPが必要。
+# Is prime?
+入力された数が素数かどうかを判定します。  
+判定器にはミラーラビン素数判定法を用いています。https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test  
+デフォルトでは100回判定されます。
+
+## Install
+多倍長整数の演算にはGNU Multi Precision Arithmetic Library(https://gmplib.org/ )を使っています。  
+macの場合は
+```
+$ brew insatll gmp
+```  
+などでインストールできます。Linuxの場合は上記サイトからアーカイブをDLし、makeでインストールできます。  
+
+あとはmakeしてください。
+
+## Usage
+### Primality test
+以下のようにコマンドラインから入力します。  
+```
+$ ./isprime.out 89
+```
+素数ならその数をそのまま返し、そうでないなら何も返しません。
+```
+$ ./isprime.out 89
+89
+$ ./isprime.out 57
+$
+```
+上のような感じになります。
+
+### Enumerate
+Primality testに一つオプションをつけることができます。オプションは整数で、正の数が入力された場合には正の方向に、負数の場合には負の方向に素数を絶対値の数だけ列挙します。0の場合は何も返しません。
+```
+$ ./isprime.out 1 25
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+$ ./isprime.out 100 -25
+97
+89
+83
+79
+73
+71
+67
+61
+59
+53
+47
+43
+41
+37
+31
+29
+23
+19
+17
+13
+11
+7
+5
+3
+2
+$
+```
+のような感じです。ある合成数nの次の素数が知りたければ、`./isprime n 1`と入力すれば表示されます。  
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ static const int k = 100;
 static const int base = 10;
 static gmp_randclass rnd(gmp_randinit_default);
 
-mpz_class modexp(mpz_class base, mpz_class exp, const mpz_class &mod){
+inline mpz_class modexp(mpz_class base, mpz_class exp, const mpz_class &mod){
 	mpz_class result = 1;
 	while (exp != 0){
 		if ((exp & 1) == 1){
@@ -22,14 +22,14 @@ mpz_class modexp(mpz_class base, mpz_class exp, const mpz_class &mod){
 	return result;
 }
 
-bool primality(const mpz_class n) {
-	if (n == 1) return false;
+inline bool primality(const mpz_class n) {
+	if (n < 2) return false;
 	if (n == 2) return true;
 	if ((n&1) == 0) return false;
 	const mpz_class m = n-1;
 	const int s = mpz_scan1(m.get_mpz_t(), 0);
 	const mpz_class d = n >> s;
-	for (size_t i = 0; i < k; i++) {
+	for (int i = 0; i < k; i++) {
 		const mpz_class a = rnd.get_z_range(n-2)+1;
 		mpz_class t = d;
 		mpz_class y = modexp(a, t, n);
@@ -45,20 +45,38 @@ bool primality(const mpz_class n) {
 }
 
 int main(const int argc, const char *argv[]){
-	if (argc < 2) return 0;
-	mpz_class n;
-	mpz_set_str(n.get_mpz_t(), argv[1], 10);
-	if (n < 2) {
-		std::cout << "composite\n";
-		return 0;
-	}
-	switch (primality(n)) {
-		case 0:
-			std::cout << "composite\n";
+	switch (argc){
+		case 2:
+		{
+			mpz_class n;
+			mpz_set_str(n.get_mpz_t(), argv[1], base);
+			if (primality(n)){
+				std::cout << n << std::endl;
+			}
 			break;
-		case 1:
-			std::cout << "prime\n";
+		}
+		case 3:
+		{
+			mpz_class n;
+			mpz_set_str(n.get_mpz_t(), argv[1], base);
+			mpz_class count;
+			mpz_set_str(count.get_mpz_t(), argv[2], base);
+			if (count==0)break;
+			int inc;
+			if (count < 0){
+				inc = -1;
+			}else{
+				inc = 1;
+			}
+			for (int i = 0; i < std::abs(count); n+=inc){
+				if (n < 2 && inc < 0)break;
+				if (primality(n)){
+					std::cout << n << std::endl;
+					i++;
+				}
+			}
 			break;
+		}
 		default:
 			break;
 	}
